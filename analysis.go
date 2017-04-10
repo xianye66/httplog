4a4b4e98390c36ecb14a0925f5e8b065f14baf7f
kevin-zx <519754821@qq.com>
2017-04-10 09:39:26 +0800

    test

---
+++ analysis.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/csv"
+	"os"
+	"io"
+	"strings"
+)
+
+type spiderinfo struct {
+	spiderName string
+	crawlerTimes int
+	EndDateTime string
+	startDateTime string
+	date string
+
+}
+
+
+
+type statistics struct {
+
+}
+
+var spiderMap = map[string]*spiderinfo{}
+
+func main(){
+	file,err := os.Open("logdata/access_log")
+	if err != nil{
+		print(err.Error())
+	}
+	defer file.Close()
+	reader :=csv.NewReader(file)
+	reader.Comma = ' '
+	for   {
+		record,err := reader.Read()
+		analysisSingleRecord(record)
+
+		if err == io.EOF{
+			break
+		}
+	}
+	println(spiderMap["Baiduspider"].spiderName, spiderMap["Baiduspider"].crawlerTimes,spiderMap["Baiduspider"].date,spiderMap["Baiduspider"].startDateTime,spiderMap["Baiduspider"].EndDateTime)
+	println(spiderMap["Googlebot"].spiderName, spiderMap["Googlebot"].crawlerTimes,spiderMap["Googlebot"].date,spiderMap["Googlebot"].startDateTime,spiderMap["Googlebot"].EndDateTime)
+}
+
+func analysisSingleRecord(record []string)  {
+	if len(record) == 10{
+		judgeSpider(record[9],strings.Replace(record[3] ,"[","",-1))
+
+	}else {
+		println("adfasd")
+	}
+}
+
+func judgeSpider(userAgent string,date string)  {
+	dateString := strings.Split(date,":")[0]
+	if strings.Contains(userAgent,"Baiduspider") {
+		test("Baiduspider", dateString, date)
+	}
+	if strings.Contains(userAgent,"Googlebot") {
+		test("Googlebot", dateString, date)
+	}
+	
+}
+
+func test(spider string, dateString string, date string)  {
+	if spiderMap[spider] == nil {
+		spiderMap[spider] = &spiderinfo{spiderName:spider,startDateTime:date, date:dateString, }
+	}else{
+		if spiderMap[spider].date != dateString {
+			println(spiderMap[spider].spiderName, spiderMap[spider].crawlerTimes,spiderMap[spider].date,spiderMap[spider].startDateTime,spiderMap[spider].EndDateTime)
+			spiderMap[spider] = &spiderinfo{spiderName:spider,startDateTime:date, date:dateString }
+		}else{
+			(spiderMap[spider]).EndDateTime = date
+			(spiderMap[spider]).crawlerTimes += 1
+		}
+	}
+}
